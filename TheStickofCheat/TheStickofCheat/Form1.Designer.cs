1932a028603d29bcff861586b1260acd02d9f5e8
Snaacky <snaacky@users.noreply.github.com>
2015-07-03 07:18:04 -0400

    The code has arrived to the party

---
+++ TheStickofCheat/TheStickofCheat/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace TheStickofCheat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnMaxLevel = new System.Windows.Forms.Button();
+            this.btnMaxMoney = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(461, 117);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnMaxLevel);
+            this.groupBox1.Controls.Add(this.btnMaxMoney);
+            this.groupBox1.Location = new System.Drawing.Point(12, 123);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(437, 71);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Options";
+            // 
+            // btnMaxLevel
+            // 
+            this.btnMaxLevel.Location = new System.Drawing.Point(6, 24);
+            this.btnMaxLevel.Name = "btnMaxLevel";
+            this.btnMaxLevel.Size = new System.Drawing.Size(200, 28);
+            this.btnMaxLevel.TabIndex = 1;
+            this.btnMaxLevel.Text = "Max Level";
+            this.btnMaxLevel.UseVisualStyleBackColor = true;
+            this.btnMaxLevel.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnMaxMoney
+            // 
+            this.btnMaxMoney.Location = new System.Drawing.Point(231, 24);
+            this.btnMaxMoney.Name = "btnMaxMoney";
+            this.btnMaxMoney.Size = new System.Drawing.Size(200, 28);
+            this.btnMaxMoney.TabIndex = 0;
+            this.btnMaxMoney.Text = "Max Money";
+            this.btnMaxMoney.UseVisualStyleBackColor = true;
+            this.btnMaxMoney.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 201);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "South Park: The Stick of Truth";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnMaxMoney;
+        private System.Windows.Forms.Button btnMaxLevel;
+    }
+}
---
+++ TheStickofCheat/TheStickofCheat/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Binarysharp.MemoryManagement;
+using Binarysharp.MemoryManagement.Helpers;
+
+namespace TheStickofCheat
+{
+
+    public partial class Form1 : Form
+    {
+        string gameName = "South Park - The Stick of Truth";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Max Money
+            using (var m = new MemorySharp(ApplicationFinder.FromProcessName(gameName).FirstOrDefault()))
+            {
+
+                var moneyPtr = new IntPtr(0x01B71110);
+                int[] offsets = { 0x0, 0x14, 0x14, 0x218 };
+                moneyPtr = m[moneyPtr].Read<IntPtr>();
+                moneyPtr = m[moneyPtr + offsets[0], false].Read<IntPtr>();
+                moneyPtr = m[moneyPtr + offsets[1], false].Read<IntPtr>();
+                moneyPtr = m[moneyPtr + offsets[2], false].Read<IntPtr>();
+                m[moneyPtr + offsets[3], false].Write<int>(999999999);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Max Level
+            using (var m = new MemorySharp(ApplicationFinder.FromProcessName(gameName).FirstOrDefault()))
+            {
+
+                var levelPtr = new IntPtr(0x01CAC924);
+                int[] offsets = { 0x324, 0x38 };
+                levelPtr = m[levelPtr].Read<IntPtr>();
+                levelPtr = m[levelPtr + offsets[0], false].Read<IntPtr>();
+                m[levelPtr + offsets[1], false].Write<int>(15);
+            }
+
+        }
+
+    }
+}
